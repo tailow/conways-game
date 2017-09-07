920badf3f98a27793da523f3cd8f751ffa9150cd
tailow-laptop <revitate@hotmail.com>
2017-09-07 22:51:56 +0300

    Add check active blocks

--- ConwaysGameOfLife/Assets/Scripts/GameManager.cs
+++ ConwaysGameOfLife/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
@@ -12,7 +13,10 @@ public class GameManager : MonoBehaviour {
     Vector2 amountOfBlocks;
 
     public GameObject blockPrefab;
+
     GameObject[,] blockArray;
+
+    List<GameObject> activeBlocksList = new List<GameObject>();
 
     #endregion
 
@@ -45,11 +49,26 @@ public class GameManager : MonoBehaviour {
         StopCoroutine("StartGameCoroutine");
     }
 
+    void CheckAllActiveBlocks()
+    {
+        activeBlocksList.Clear();
+
+        foreach (GameObject block in blockArray)
+        {
+            if (block.GetComponent<Image>().color == Color.black)
+            {
+                activeBlocksList.Add(block);
+            }
+        }
+    }
+
     IEnumerator StartGameCoroutine()
     {
+        CheckAllActiveBlocks();
+
         for (int i = 0; i < Mathf.Infinity; i++)
         {
-            Debug.Log(i);
+            //Debug.Log(i);
 
             yield return new WaitForSeconds(0.5f);
         }      
@@ -57,15 +76,23 @@ public class GameManager : MonoBehaviour {
 
     void GenerateBlocks()
     {
+        int blockNumberX = 0;
+        int blockNumberY = 0;
+
         for (int y = 15; y < Screen.height - topBarSize; y += (int)blockSize.y)
-        {
+        {     
             for (int x = 15; x < Screen.width; x += (int)blockSize.x)
             {
                 var blockObj = Instantiate(blockPrefab, new Vector2(x, y), Quaternion.identity);
 
                 blockObj.transform.SetParent(GameObject.Find("Blocks").transform);
+                blockArray[blockNumberX, blockNumberY] = blockObj;
 
+                blockNumberX++;
             }
+
+            blockNumberY++;
+            blockNumberX = 0;
         }
     }
 }
