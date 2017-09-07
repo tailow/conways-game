de210d6dbd68928440e6ef2ff300b3d9a3942b59
tailow-laptop <revitate@hotmail.com>
2017-09-07 21:51:46 +0300

    Add block generation and button functionality

---
+++ ConwaysGameOfLife/Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour {
+
+	public void ToggleButtonColor()
+    {
+        Color currentColor = GetComponent<Image>().color;
+
+        if (currentColor == Color.white)
+        {
+            GetComponent<Image>().color = Color.black;
+        }
+
+        else if (currentColor == Color.black)
+        {
+            GetComponent<Image>().color = Color.white;
+        }
+
+        else
+        {
+            Debug.Log("Failed to check for current color.");
+        }
+    }
+}
---
+++ ConwaysGameOfLife/Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    #region Variables
+
+    Vector2 blockSize;
+    Vector2 amountOfBlocks;
+
+    public GameObject blockPrefab;
+    GameObject[,] blockArray;
+
+    #endregion
+
+    void Start()
+    {
+        blockSize = new Vector2(30, 30);
+        amountOfBlocks = new Vector2(Screen.width/blockSize.x, Screen.height/blockSize.y);
+
+        blockArray = new GameObject[(int)amountOfBlocks.x, (int)amountOfBlocks.y];
+
+        GenerateBlocks();
+    }
+
+    void GenerateBlocks()
+    {
+        for (int y = 15; y < Screen.height; y += (int)blockSize.y)
+        {
+            for (int x = 15; x < Screen.width; x += (int)blockSize.x)
+            {
+                var blockObj = Instantiate(blockPrefab, new Vector2(x, y), Quaternion.identity);
+
+                blockObj.transform.SetParent(GameObject.Find("Blocks").transform);
+
+            }
+        }
+    }
+}
